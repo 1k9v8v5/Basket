7b208fb8728d03ea0238689ac9659a16eaad56f0
1k9v8v5 <1k9v8v5@mail.ru>
2016-05-05 22:48:11 +0400

    basket

--- Basket/app/src/main/java/com/example/kv/basket/Basket.java
+++ Basket/app/src/main/java/com/example/kv/basket/Basket.java
@@ -1,1 +1,13 @@
-package com.example.kv.basket;import android.support.v7.app.AppCompatActivity;import android.os.Bundle;public class Basket extends AppCompatActivity {    @Override    protected void onCreate(Bundle savedInstanceState) {        super.onCreate(savedInstanceState);        setContentView(R.layout.activity_basket);    }}
+package com.example.kv.basket;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Basket extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basket);
+    }
+}
