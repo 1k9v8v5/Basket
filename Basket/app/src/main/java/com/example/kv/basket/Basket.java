faea4a28956352228d46c60a1dc6e137edc53706
1k9v8v5 <1k9v8v5@mail.ru>
2016-05-05 22:42:18 +0400

    basket

---
+++ Basket/app/src/main/java/com/example/kv/basket/Basket.java
@@ -0,0 +1,1 @@
+package com.example.kv.basket;import android.support.v7.app.AppCompatActivity;import android.os.Bundle;public class Basket extends AppCompatActivity {    @Override    protected void onCreate(Bundle savedInstanceState) {        super.onCreate(savedInstanceState);        setContentView(R.layout.activity_basket);    }}
