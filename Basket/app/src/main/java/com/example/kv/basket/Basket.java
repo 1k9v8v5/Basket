f7ed128fe849e896fb993edb36657bc7f1f4874f
1k9v8v5 <1k9v8v5@mail.ru>
2016-05-06 20:12:40 +0400

    basket

--- Basket/app/src/main/java/com/example/kv/basket/Basket.java
+++ Basket/app/src/main/java/com/example/kv/basket/Basket.java
@@ -1,9 +1,10 @@
 package com.example.kv.basket;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Basket extends AppCompatActivity {
+public class Basket extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
