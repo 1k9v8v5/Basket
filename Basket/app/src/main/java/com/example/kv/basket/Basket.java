2427c67c0a24558f7098e048c6c1ad7e9b1c6b97
1k9v8v5 <1k9v8v5@mail.ru>
2016-05-08 10:09:24 +0400

    1

--- Basket/app/src/main/java/com/example/kv/basket/Basket.java
+++ Basket/app/src/main/java/com/example/kv/basket/Basket.java
@@ -1,13 +1,38 @@
 package com.example.kv.basket;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class Basket extends AppCompatActivity {
+
+public class Basket extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basket);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_list);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_create_list:
+
+                return true;
+            case R.id.action_settings:
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
